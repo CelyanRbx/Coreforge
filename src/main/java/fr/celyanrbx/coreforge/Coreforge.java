1b425a2c442c194597e22dc3946d261ad3f5611c
CelyanRbx <pro.bastosofficiel@gmail.com>
2024-11-17 22:08:00 +0100

    Changed some things

--- src/main/java/fr/celyanrbx/coreforge/Coreforge.java
+++ src/main/java/fr/celyanrbx/coreforge/Coreforge.java
@@ -1,6 +1,7 @@
 package fr.celyanrbx.coreforge;
 
 import fr.celyanrbx.coreforge.block.ModBlocks;
+import fr.celyanrbx.coreforge.item.ModCreativeModeTabs;
 import fr.celyanrbx.coreforge.item.ModItems;
 import org.slf4j.Logger;
 
@@ -49,6 +50,8 @@ public class Coreforge {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
