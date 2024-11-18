df3eb91c622438e20eaf57dbc9d8191b87ba2225
CelyanRbx <pro.bastosofficiel@gmail.com>
2024-11-18 13:37:04 +0100

    Add Magic Block

---
+++ src/main/java/fr/celyanrbx/coreforge/block/custom/MagicBlock.java
@@ -0,0 +1,57 @@
+package fr.celyanrbx.coreforge.block.custom;
+
+import fr.celyanrbx.coreforge.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+
+public class MagicBlock extends Block {
+
+    protected static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 12.0, 16.0);
+
+    public MagicBlock(Properties properties) {
+        super(properties.strength(1.8F).noOcclusion());
+    }
+
+    @Override
+    protected @NotNull VoxelShape getShape(@NotNull BlockState sate, @NotNull BlockGetter level, @NotNull
+    BlockPos pos, @NotNull CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos,
+                                               Player player, BlockHitResult hitResult) {
+        level.playSound(player, pos, SoundEvents.AMETHYST_CLUSTER_PLACE, SoundSource.BLOCKS, 1f, 1f);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if(entity instanceof ItemEntity itemEntity) {
+            if(itemEntity.getItem().getItem() == ModItems.STEEL.get()) {
+                itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
+            }
+
+            if(itemEntity.getItem().getItem() == Items.DANDELION) {
+                itemEntity.setItem(new ItemStack(Items.WITHER_ROSE, itemEntity.getItem().getCount()));
+            }
+        }
+
+        super.stepOn(level, pos, state, entity);
+    }
+}
