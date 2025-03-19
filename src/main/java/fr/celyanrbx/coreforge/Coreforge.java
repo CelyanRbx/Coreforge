c24a7e3063f84bd3fc16ed95b71e7d8c1bde0656
CelyanRbx <glmtjulie@gmail.com>
2025-03-19 03:09:47 +0100

    Initial commit

---
+++ src/main/java/fr/celyanrbx/coreforge/Coreforge.java
@@ -0,0 +1,10 @@
+package fr.celyanrbx.coreforge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Coreforge.MODID)
+public class Coreforge {
+
+    public static final String MODID = "coreforge";
+
+}
