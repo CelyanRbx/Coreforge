c8872fe0adb0d3111e79ab362ccf146ac06192ef
CelyanRbx <glmtjulie@gmail.com>
2025-03-24 12:29:22 +0100

    add Mc and Create (Crushing and Milling)

---
+++ src/main/java/fr/celyanrbx/crushblock/CrushBlock.java
@@ -0,0 +1,10 @@
+package fr.celyanrbx.crushblock;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(CrushBlock.MODID)
+public class CrushBlock {
+
+    public static final String MODID = "crushblock";
+
+}
